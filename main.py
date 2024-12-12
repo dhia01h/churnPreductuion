f648c370fa35e1d14b7e7eebc8226e980624c652
dhia01h <148395738+dhia01h@users.noreply.github.com>
2024-12-12 12:40:48 +0100

    dahia

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
-import joblib
+import jobliba
 
 # Initialiser l'application FastAPI
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Remplacez "*" par une liste des origines autorisées, ex: ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Autoriser toutes les méthodes (GET, POST, etc.)
+    allow_headers=["*"],  # Autoriser tous les en-têtes
+)
 
 # Charger les modèles et encodeurs sauvegardés
 state_encoder = joblib.load('state_encoder.pkl')
